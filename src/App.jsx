34bcc856881237824a780255c178ed5192c9fc4e
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2025-03-30 23:20:22 +0800

    add Tailwind Css.

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { FaBeer } from 'react-icons/fa';
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div className='bg-red-500'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,7 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1><FaBeer></FaBeer></h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
