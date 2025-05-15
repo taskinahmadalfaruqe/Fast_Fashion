bcb973e217c298c32f57f8de8b76d6f6a733efae
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2025-05-15 22:18:13 +0800

    Add Common Button.

---
+++ src/Component/SharedComponent/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+
+import CommonButton from '../Button/CommonButton';
+
+const Footer = () => {
+    return (
+        <div>
+            This IS Footer.
+            <CommonButton ButtonName={"BDT"} NavigationLink={'/ProductPage'}></CommonButton>
+        </div>
+    );
+};
+
+export default Footer;
--- src/Layoute/MainLayoute/MainLayoute.jsx
+++ src/Layoute/MainLayoute/MainLayoute.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import Navbar from '../../Component/SharedComponent/Navbar';
-import Footer from '../../Component/SharedComponent/Footer';
+
 import { Outlet } from 'react-router-dom';
+import Footer from '../../Component/SharedComponent/Footer/Footer';
 
 const MainLayoute = () => {
     return (
         <div>
-            <Navbar></Navbar>
+
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
