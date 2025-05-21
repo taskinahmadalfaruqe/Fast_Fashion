ae15faf4185c7d0a944995156d239b66a33edf77
Mostaq27 <mostaq99muzahid@gmail.com>
2025-05-22 05:49:07 +0600

    edit journal page

--- src/Page/HomePage/HomePage.jsx
+++ src/Page/HomePage/HomePage.jsx
@@ -1,6 +1,7 @@
 import CommonButton from '../../Component/SharedComponent/Button/CommonButton';
 import CommonHedding from '../../Component/SharedComponent/Hedding/CommonHedding';
 import FindPathname from '../../Hooks/FindPathname/FindPathname';
+// import Carousel from '../../Component/Slider/Carousel' 
 
 const HomePage = () => {
     return (
@@ -8,6 +9,10 @@ const HomePage = () => {
             <CommonHedding FastPart={"Ho"} SecondPart={"me"} PageLink={"/"}></CommonHedding>
             <FindPathname></FindPathname>
             <CommonButton ButtonName={"Home"} NavigateLink={"/"}></CommonButton>
+            
+            {/* <div className='h-72 w-full mt-3'>
+               <Carousel></Carousel>
+            </div> */}
         </div >
     );
 };
--- src/Page/Journal/Journal.jsx
+++ src/Page/Journal/Journal.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import CommonHedding from '../../Component/SharedComponent/Hedding/CommonHedding';
+import FindPathname from '../../Hooks/FindPathname/FindPathname';
+import CommonButton from '../../Component/SharedComponent/Button/CommonButton';
 
 const Journal = () => {
     return (
-        <div>
-            this is Journal page
+        <div className='container'>
+            <CommonHedding FastPart={"Jour"} SecondPart={"nal"} PageLink={"/"}/>
+            <FindPathname/>
+            <CommonButton ButtonName={"Home"} NavigateLink={"/"} />
         </div>
     );
 };
