5361d13d5219f90a9c1c238e8bc4caa4249ad40d
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2025-05-22 23:36:21 +0800

    [Button](Devtaskin): Fisxed Common Button Color Combination.

--- src/Component/HomePage/HomepageAdd/SaleBanner.jsx
+++ src/Component/HomePage/HomepageAdd/SaleBanner.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import CommonButton from "../../SharedComponent/Button/CommonButton";
+import phone from './../../../Assests/SellBord/phone.jpg';
+import electronics from './../../../Assests/SellBord/electronics.jpg';
+import furniture from './../../../Assests/SellBord/furniture.jpeg';
 
 const SaleBanner = () => {
   const sales = [
@@ -7,26 +10,26 @@ const SaleBanner = () => {
       title: "Phones Sale",
       discount: "30%",
       description: "Up to 30% sale for all phones!",
-      image: "./../../../Assests/SellBord/phone.jpg",
+      image: { phone },
     },
     {
       title: "Electronics Sale",
       discount: "70%",
       description: "Up to 70% sale for all electronics!",
-      image: "./../../../Assests/SellBord/electronics.jpg",
+      image: { electronics },
     },
     {
       title: "Furniture Offer",
       discount: "50%",
       description: "Up to 50% sale for all furniture items!",
-      image: "./../../../Assests/SellBord/furniture.jpeg",
+      image: { furniture },
     },
   ];
 
   return (
     <div className="bg-[#f5f5f5] dark:bg-[#262626] py-8 min-h-screen">
       <div className="max-w-6xl mx-auto px-4">
-        
+
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <div className="bg-white dark:bg-gray-800 p-6 rounded-lg flex flex-col items-start justify-between shadow">
             <img src={sales[0].image} alt={sales[0].title} className="w-full h-60 object-contain mb-4" />
--- src/Component/SharedComponent/Button/CommonButton.jsx
+++ src/Component/SharedComponent/Button/CommonButton.jsx
@@ -5,10 +5,10 @@ const CommonButton = ({ ButtonName, NavigateLink }) => {
   return (
     <Link
       to={NavigateLink}
-      className="relative inline-flex items-center justify-center px-2 md:px-6 py-3 overflow-hidden font-medium transition-all bg-White_Color rounded hover:bg-Blue_color dark:bg-Blue_color  group border-[1px] border-Primary_Color"
+      className="relative inline-flex items-center justify-center px-2 md:px-6 py-3 overflow-hidden font-medium transition-all bg-Primary_Color rounded hover:bg-Blue_color dark:bg-Blue_color  group border-[1px] border-Black_Color dark:border-White_Color text-Black_Color dark:text-White_Color hover:text-White_Color dark:hover:text-Blue_color"
     >
-      <span className="w-48 h-48 rounded rotate-[-40deg] bg-Primary_Color absolute bottom-0 left-0 -translate-x-full ease-out duration-1000 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
-      <span className="relative w-full text-center transition-colors duration-400 ease-in-out dark:group-hover:text-Blue_color text-base lg:text-sm xl:text-base">
+      <span className="w-48 h-48 rounded rotate-[-40deg] bg-Black_Color dark:bg-White_Color absolute bottom-0 left-0 -translate-x-full ease-out duration-1000 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+      <span className="relative w-full text-center transition-colors duration-400 ease-in-out text-base lg:text-sm xl:text-base">
         {ButtonName}
       </span>
     </Link>
