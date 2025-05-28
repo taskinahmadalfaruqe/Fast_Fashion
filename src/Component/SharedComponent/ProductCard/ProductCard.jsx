1c0f7ce5dff8abfd8490b644ffb190344fa1f26d
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2025-05-28 23:20:34 +0800

    [ProductCard](devTaskin):Add Product card OverFlow.

--- src/Component/SharedComponent/ProductCard/ProductCard.jsx
+++ src/Component/SharedComponent/ProductCard/ProductCard.jsx
@@ -1,8 +1,10 @@
+import { MdFavoriteBorder } from 'react-icons/md';
 import pic from '/phone.jpg'
+import { FaCartShopping, FaCodeCompare } from 'react-icons/fa6';
 const ProductCard = () => {
     return (
         <div>
-            <div className='flex gap-5 flex-col max-w-60'>
+            <div className='flex gap-5 flex-col max-w-60 relative ProductCard'>
                 <div className='bg-Card_Background_Color w-60 h-60 flex justify-center items-center p-5'>
                     <img src={pic} alt="Product Image" />
                 </div>
@@ -11,10 +13,30 @@ const ProductCard = () => {
                         <h3>Product Name</h3>{/*product color  */}
                         <span>$88</span>{/*product price  */}
                     </div>
-                     <p>Black</p> {/*product color  */}
+                    <p>Black</p> {/*product color  */}
+                </div>
+
+            </div>
+            <div className='text-Gray_Color bg-White_Color absolute bottom-0 left-0 w-full hidden ProductCardOverflow'>
+                <div className='flex gap-2 p-2 justify-end items-center hover:text-Black_Color   cursor-pointer'>
+                    <span>Add to Wish List</span>
+                    <span>
+                        < MdFavoriteBorder></MdFavoriteBorder>
+                    </span>
+                </div>
+                <div className='flex gap-2 p-2 justify-end items-center hover:text-Black_Color   cursor-pointer'>
+                    <span>Compare</span>
+                    <span>
+                        <FaCodeCompare></FaCodeCompare>
+                    </span>
+                </div>
+                <div className='flex gap-2 p-2 justify-end items-center hover:text-Black_Color   cursor-pointer'>
+                    <span>Add to Cart</span>
+                    <span>
+                        <FaCartShopping></FaCartShopping>
+                    </span>
                 </div>
             </div>
-            <div></div>
         </div>
     );
 };
