4d058b945efc1b2311d31425261140935bfd705f
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2025-05-29 22:39:38 +0800

    [UpdateProductCard](devTaskin):Add Hover Effect at procduct card.

--- src/Component/SharedComponent/ProductCard/ProductCard.jsx
+++ src/Component/SharedComponent/ProductCard/ProductCard.jsx
@@ -1,40 +1,36 @@
 import { MdFavoriteBorder } from 'react-icons/md';
-import pic from '/phone.jpg'
+import pic from '/phone.jpg';
 import { FaCartShopping, FaCodeCompare } from 'react-icons/fa6';
+
 const ProductCard = () => {
     return (
-        <div>
-            <div className='flex gap-5 flex-col max-w-60 relative ProductCard'>
+        <div className="relative max-w-60 group">
+            <div className='flex gap-5 flex-col ProductCardOverflow'>
                 <div className='bg-Card_Background_Color w-60 h-60 flex justify-center items-center p-5'>
                     <img src={pic} alt="Product Image" />
                 </div>
                 <div>
                     <div className='flex justify-between items-center'>
-                        <h3>Product Name</h3>{/*product color  */}
-                        <span>$88</span>{/*product price  */}
+                        <h3>Product Name</h3>
+                        <span>$88</span>
                     </div>
-                    <p>Black</p> {/*product color  */}
+                    <p>Black</p>
                 </div>
-
             </div>
-            <div className='text-Gray_Color bg-White_Color absolute bottom-0 left-0 w-full hidden ProductCardOverflow'>
-                <div className='flex gap-2 p-2 justify-end items-center hover:text-Black_Color   cursor-pointer'>
+
+            {/* Hover Menu with Transitions */}
+            <div className='text-Gray_Color bg-White_Color absolute bottom-0 left-0 w-full opacity-0 translate-y-4 pointer-events-none group-hover:opacity-100 group-hover:translate-y-0 group-hover:pointer-events-auto transition-all duration-300 ease-in-out flex flex-col'>
+                <div className='flex gap-2 p-2 justify-end items-center hover:text-Black_Color cursor-pointer'>
                     <span>Add to Wish List</span>
-                    <span>
-                        < MdFavoriteBorder></MdFavoriteBorder>
-                    </span>
+                    <MdFavoriteBorder />
                 </div>
-                <div className='flex gap-2 p-2 justify-end items-center hover:text-Black_Color   cursor-pointer'>
+                <div className='flex gap-2 p-2 justify-end items-center hover:text-Black_Color cursor-pointer'>
                     <span>Compare</span>
-                    <span>
-                        <FaCodeCompare></FaCodeCompare>
-                    </span>
+                    <FaCodeCompare />
                 </div>
-                <div className='flex gap-2 p-2 justify-end items-center hover:text-Black_Color   cursor-pointer'>
+                <div className='flex gap-2 p-2 justify-end items-center hover:text-Black_Color cursor-pointer'>
                     <span>Add to Cart</span>
-                    <span>
-                        <FaCartShopping></FaCartShopping>
-                    </span>
+                    <FaCartShopping />
                 </div>
             </div>
         </div>
