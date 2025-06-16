dc3d731ad78707b4d6414f45993979b25e98f4a7
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2025-06-16 23:55:31 +0800

    [ProductOfThe Year](devTaskin):Add Second Part Of product of the Year.

--- src/Component/HomePage/ProductOfTheYear/ProductOfTheYear.jsx
+++ src/Component/HomePage/ProductOfTheYear/ProductOfTheYear.jsx
@@ -1,3 +1,4 @@
+import CommonButton from '../../SharedComponent/Button/CommonButton';
 import phone from '/phone.jpg';
 const ProductOfTheYear = () => {
     return (
@@ -5,8 +6,10 @@ const ProductOfTheYear = () => {
             <div className='h-full'>
                 <img className='h-full' src={phone} alt="BestProduct" />
             </div>
-            <div>
-
+            <div className='flex flex-col gap-5 justify-between items-start max-w-xl'>
+                <h2>Phone of the year</h2>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry orem Ipsum..</p>
+                <CommonButton ButtonName={"Shop Now"} NavigateLink={"/"}></CommonButton>
             </div>
         </div>
     );
