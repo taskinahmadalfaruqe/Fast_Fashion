7da998f704f5defe34d86c3724fa4f6b746dd7a0
TASKIN AHMAD AL FARUQE <taskinahmed180@gmail.com>
2025-07-19 23:11:48 +0800

    [searchBox](devTaskin): Try to add Serach box design.

--- src/Component/SharedComponent/SearchNavbar/SearchNavbar.jsx
+++ src/Component/SharedComponent/SearchNavbar/SearchNavbar.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import FindPathname from '../../../Hooks/FindPathname/FindPathname'
 import CommonButton from '../Button/CommonButton'
+import { FaBars } from 'react-icons/fa';
 
 function SearchNavbar() {
     return (
-        <div>
-            <FindPathname></FindPathname>
-            <CommonButton ButtonName={"Shop"}></CommonButton>
+        <div className='container'>
+            <div className='CategoryBar'>
+                <FaBars />
+                <span>Shop By Category</span>
+            </div>
+            <div className='Searcbox'>
+                <div className='text-Black_Color'>
+                    <input type="text" placeholder="Type here" className="input" />
+                </div>
+            </div>
+            <div></div>
         </div>
     )
 }
 
-export default SearchNavbar
+export default SearchNavbar;
--- src/Layoute/MainLayoute/MainLayoute.jsx
+++ src/Layoute/MainLayoute/MainLayoute.jsx
@@ -2,11 +2,13 @@
 import { Outlet } from 'react-router-dom';
 import Footer from '../../Component/SharedComponent/Footer/Footer';
 import Navbar from '../../Component/SharedComponent/Navbar/Navbar';
+import SearchNavbar from '../../Component/SharedComponent/SearchNavbar/SearchNavbar';
 
 const MainLayoute = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <SearchNavbar></SearchNavbar>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
