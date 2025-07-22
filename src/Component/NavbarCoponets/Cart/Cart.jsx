1099b2932ea2d6eb8e16ca43def5dd4a72b05eba
TASKIN AHMAD AL FARUQE <taskinahmed180@gmail.com>
2025-07-22 20:23:09 +0800

    [SearchBox Responsive](devTaskin): Finaly compete search box responsive and nevigation bar.

--- src/Component/NavbarCoponets/Cart/Cart.jsx
+++ src/Component/NavbarCoponets/Cart/Cart.jsx
@@ -4,14 +4,14 @@ import { MdShoppingCart } from "react-icons/md";
 const Cart = () => {
     return (
         <div>
-            <button className="relative group p-1 px-4 pr- cursor-pointer rounded-md bg-Orange_Color text-White_Color border-[2px] border-White_Color ">
+            <button className="relative group p-1  cursor-pointer rounded-md  text-White_Color  ">
                 <span className="">
-                    <MdShoppingCart className="text-3xl font-bold" />
+                    <MdShoppingCart className="text-3xl font-bold text-Orange_Color" />
                     <div className="absolute right-5 -bottom-10 mb-2 bg-Orange_Color text-White_Color text-xs px-2 py-1 rounded whitespace-nowrap opacity-0 group-hover:opacity-100 transition-opacity z-[1000]">
                         Show All Cart Products
                     </div>
                 </span>
-                <div className="absolute -top-2 -right-2 text-[10px] font-bold flex justify-center items-center p-2 rounded-full h-7 w-7 bg-Orange_Color text-White_Color border-[2px] border-White_Color ">
+                <div className="absolute -top-3 -right-3 text-[10px] font-bold flex justify-center items-center p-2 rounded-full h-7 w-7 bg-Orange_Color text-White_Color border-[2px] border-White_Color ">
                     100
                 </div>
             </button>
--- src/Component/SharedComponent/SearchNavbar/SearchNavbar.jsx
+++ src/Component/SharedComponent/SearchNavbar/SearchNavbar.jsx
@@ -7,11 +7,11 @@ import { Link } from 'react-router-dom';
 function SearchNavbar() {
     return (
         <div className='bg-[#f5f5f5] dark:bg-Black_Color border-b-2 border-Blue_color'>
-            <div className="container" style={{ margin: "0px auto 0px" }}>
-                <div className=' flex justify-between items-center '>
+            <div className="container" style={{ margin: "0px auto 0px", padding:" 0px 10px" }}>
+                <div className=' flex justify-between items-center gap-5 '>
                     <div className='flex justify-start items-center gap-2 '>
                         <FaBars />
-                        <span>Shop By Category</span>
+                        <span className='hidden'>Shop By Category</span>
                     </div>
                     <div className='Searcbox'>
                         <div className='flex justify-center items-center gap-3'>
@@ -23,12 +23,12 @@ function SearchNavbar() {
                     </div>
                     <div className='py-2 flex justify-center items-center gap-2'>
                         <div>
-                            <button className="btn text-xl" popoverTarget="popover-1" style={{ anchorName: "--anchor-1" } /* as React.CSSProperties */}>
+                            <button className="btn text-xl" popoverTarget="popover-1" style={{ anchorName: "--anchor-1", padding:'2px' } /* as React.CSSProperties */}>
                                 <CgProfile></CgProfile>
                                 <IoMdArrowDropdown></IoMdArrowDropdown>
                             </button>
 
-                            <ul className="dropdown menu w-32  shadow-sm bg-Primary_Color dark:bg-Blue_color rounded-box mt-3"
+                            <ul className="dropdown menu w-24  shadow-sm bg-Primary_Color dark:bg-Blue_color rounded-box mt-3"
                                 popover="auto" id="popover-1" style={{ positionAnchor: "--anchor-1", position: 'absolute', buttom: 0, right: 0 } /* as React.CSSProperties */}>
                                 <li><Link to={'/User/Login'}>Login</Link></li>
                                 <li><Link to={'/auth/newUser/signUpPage'}>Sign UP</Link></li>
