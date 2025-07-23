064445431fbd4b6f3b3d14d67f1d061ad9f0d6b7
TASKIN AHMAD AL FARUQE <taskinahmed180@gmail.com>
2025-07-23 23:51:37 +0800

    Add Product Deatials Page.

---
+++ src/Component/SharedComponent/ProductDetails/ProductDetails.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const ProductDetails = () => {
+  return (
+    <div>
+      <div className="ProductImage">
+
+      </div>
+      <div className="ProductDetails">
+        <div>Product Name</div>
+        <div>Product Rating</div>
+        <div>Product Price</div>
+        <div>Product Color</div>
+        <div>Product Sell</div>
+        <div>Product Stock</div>
+      </div>
+    </div>
+  )
+}
+
+export default ProductDetails;
